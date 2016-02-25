f0ecce59cb18a10dc7436dfd22c9214ee9b3e002
Ivan Miranda <ivan.i.n@hotmail.com>
2016-02-25 11:18:50 -0300

    project files

---
+++ test/lib/example.js
@@ -0,0 +1,5 @@
+window.QUnit.test("a basic test example", function (assert) {
+    var value = "hello";
+    assert.equal(value, "hello", "We expect value to be hello");
+    assert.ok(true, "Deu certo");
+});
